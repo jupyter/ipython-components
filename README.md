c72f2234d47bb68e13dd661620d6b17b4b8cae4b
MinRK <benjaminrk@gmail.com>
2013-04-29 16:07:55 -0700

    init with bower / non bower installs

---
+++ README.md
@@ -0,0 +1,4 @@
+# IPython javascript components
+
+Some installed with Bower, others installed with Git, because bower can't install everything.
+This is for use as a submodule in IPython.
---
+++ fabfile.py
@@ -0,0 +1,67 @@
+""" fabfile to prepare the notebook """
+
+import json
+import os
+import tempfile
+import urllib2
+import shutil
+
+from io import BytesIO
+from tarfile import TarFile
+from zipfile import ZipFile
+
+from fabric.api import local,lcd
+from fabric.utils import abort
+
+pjoin = os.path.join
+
+here = os.path.dirname(__file__)
+
+static_dir = here
+components_dir = pjoin(static_dir, 'components')
+
+def clean():
+    if os.path.exists(components_dir):
+        shutil.rmtree(components_dir)
+
+def components():
+    """install components with bower"""
+    with lcd(static_dir):
+        local('bower install')
+
+def nonbower():
+    if not os.path.exists(components_dir):
+        components()
+    
+    with open("nonbower.json") as f:
+        cfg = json.load(f)
+    for name, repo in cfg.get('dependencies', {}).items():
+        
+        clone = "git clone"
+        if '#' in repo:
+            repo, tag = repo.split('#')
+        else:
+            tag = None
+            clone += " --depth 1"
+        
+        with lcd(components_dir):
+            
+            local("{clone} {repo} {name}".format(**locals()))
+            
+            if tag:
+                with lcd(pjoin(components_dir, name)):
+                    local("git checkout -b {0} tags/{0}".format(tag))
+        
+        # remove the git tree, so we don't get submodules
+        shutil.rmtree(pjoin(components_dir, name, '.git'))
+
+def postprocess():
+    with lcd(pjoin(components_dir, "bootstrap")):
+        local("npm install")
+        local("make")
+
+def update():
+    clean()
+    components()
+    nonbower()
+    postprocess()
